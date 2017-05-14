cc95b6a8efea8ae334dfe1ac74e1eb8475705b9c
rlbagad <rahul.bagad@walchandsangli.ac.in>
2017-05-14 17:30:25 +0530

    First commit

---
+++ vector_addition.cu
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+__global__ void add(int *a,int *b,int *c)
+{
+	int tID = blockIdx.x;
+	if(tID<10)
+	{
+		c[tID]=a[tID]+b[tID];
+	}
+}
+
+
+int main()
+{
+	int a[10],b[10],c[10],i;
+	int *da,*db,*dc;
+	cudaMalloc((void **) &da, 10*sizeof(int));
+	cudaMalloc((void **) &db, 10*sizeof(int));
+	cudaMalloc((void **) &dc, 1*sizeof(int));
+	for(i=0;i<10;i++)
+	{
+		a[i]=i;
+		b[i]=2*i;
+	}
+	
+
+	cudaMemcpy(da, a, 10*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(db, b, 10*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dc, c, 10*sizeof(int), cudaMemcpyHostToDevice);
+
+	add<<<10,2>>>(da, db, dc);
+	cudaMemcpy(c, dc, 10*sizeof(int), cudaMemcpyDeviceToHost);
+	
+	for(i=0;i<10;i++)
+	{
+		printf("%d + %d = %d\n",a[i],b[i],c[i]);
+	}
+	return 0;
+}
